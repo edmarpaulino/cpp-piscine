2f7db02346344a4668c86154b6a1bb785804ef6e
Edmar Paulino <edmarpaulino9@gmail.com>
2023-04-15 16:08:03 -0300

    update: update code format style

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,22 +6,30 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 20:05:48 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/04/09 20:14:49 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/04/15 15:55:18 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	if (argc < 2)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		
+		return 0;
+	}
 
-	for (int i = 1; argv[i] != NULL; i++) {
+	for (int i = 1; i < argc; i++)
+	{
 		std::string str(argv[i]);
-		for (std::string::iterator it = str.begin(); it != str.end(); it++) {
+
+		for (std::string::iterator it(str.begin()); it != str.end(); it++)
+		{
 			*it = toupper(*it);
 		}
+
 		std::cout << str << std::endl;
 	}
 	
