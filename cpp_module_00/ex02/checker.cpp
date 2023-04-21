7cad9e38f6fb1bc3c87ffbf916fde35faec65537
Edmar Paulino <edmarpaulino9@gmail.com>
2023-04-21 18:53:13 -0300

    chore: reformated

--- cpp_module_00/ex02/checker.cpp
+++ cpp_module_00/ex02/checker.cpp
@@ -3,35 +3,46 @@
 #include <fstream>
 #include <iomanip>
 
-int main(void) {
-    std::ifstream originalLog("19920104_091532.log");
-    std::ifstream todayLog("today.log");
-
+int main(void)
+{
     int index = 1;
     int ret = 0;
     std::string originalLine("");
     std::string todayLine("");
+    std::ifstream originalLog("19920104_091532.log");
+    std::ifstream todayLog("today.log");
 
-    while (true) {
-        if (!originalLog.eof()) {
+    while (true)
+    {
+        if (!originalLog.eof())
+        {
             std::getline(originalLog, originalLine);
         }
 
-        if (!todayLog.eof()) {
+        if (!todayLog.eof())
+        {
             std::getline(todayLog, todayLine);
         }
 
-        if (originalLog.eof() || todayLog.eof()) {
+        if (originalLog.eof() || todayLog.eof())
+        {
             break;
         }
         
         while (!originalLine.empty() && *originalLine.begin() != ' ')
+        {
             originalLine.erase(originalLine.begin());
+        }
+
         while (!todayLine.empty() && *todayLine.begin() != ' ')
+        {
             todayLine.erase(todayLine.begin());
+        }
 
         if (originalLine != todayLine)
+        {
             ret = 1;
+        }
 
         std::cout 
             << std::setfill('0')
@@ -48,9 +59,14 @@ int main(void) {
         index++;
     }
 
-    if (!(originalLog.eof() && todayLog.eof())) {
-        std::cout << "The files doesn't have the same number of lines" << std::endl;
+    if (!(originalLog.eof() && todayLog.eof()))
+    {
+        std::cout
+            << "The files doesn't have the same number of lines"
+            << std::endl;
+
         ret = 1;
     }
+
     return ret;
 }
