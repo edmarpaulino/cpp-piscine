1b213b765d82213dc28c227b144ae00010832f27
Edmar Paulino <edmarpaulino9@gmail.com>
2023-07-30 20:49:46 -0300

    chore: add more tests mdoule 04 ex 01

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 21:36:46 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/07/30 19:56:05 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/07/30 20:45:57 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,130 @@
 #include "WrongCat.hpp"
 #include "Brain.hpp"
 
-void printSeparator(void);
+static void testDeepCopy(void);
+static void printBabyChickSeparator(void);
+static void printTreeSeparator(void);
+static void printFireSeparator(void);
 
 int main(void) {
-  Brain brain;
+  Animal *animals[10];
 
-  std::cout << brain << std::endl;
+  printBabyChickSeparator();
 
-  printSeparator();
+  for (unsigned int i = 0; i < 5; i++) {
+    animals[i] = new Dog;
+    printTreeSeparator();
+  }
 
-  Brain brain2(brain);
+  printBabyChickSeparator();
 
-  printSeparator();
+  for (unsigned int i = 5; i < 10; i++) {
+    animals[i] = new Cat;
+    printTreeSeparator();
+  }
 
-  brain.setIdea(42 - 1, "Marvin");
-  brain2.setIdea(21 - 1, "Mar");
+  printBabyChickSeparator();
 
-  brain.getIdea(420);
-  brain2.setIdea(420, "Everything");
+  for (unsigned int i = 0; i < 10; i++) {
+    delete animals[i];
+    printTreeSeparator();
+  }
 
-  printSeparator();
+  printFireSeparator();
 
-  std::cout << brain << std::endl;
-
-  printSeparator();
+  testDeepCopy();
   
-  std::cout << brain2 << std::endl;
-
-  printSeparator();
+  printFireSeparator();
 
 	return 0;
 }
 
-void printSeparator(void) {
+static void testDeepCopy(void) {
+  Dog *snoopy = new Dog;
+
+  printTreeSeparator();
+
+  Dog *scooby = new Dog(*snoopy);
+
+  printBabyChickSeparator();
+  
+  Brain *snoopyBrain = snoopy->getBrain();
+  Brain *scoobyBrain = scooby->getBrain();
+
+  snoopyBrain->setIdea(42 - 1, "Moulinette");
+  scoobyBrain->setIdea(42 - 1, "Norminette");
+
+  std::cout
+    << "🚀 Snoopy brain - address: " << snoopyBrain
+    << " - 42 idea: " << snoopyBrain->getIdea(42 - 1)
+    << std::endl;
+
+  std::cout
+    << "🚀 Scooby brain - address: " << scoobyBrain
+    << " - 42 idea: " << scoobyBrain->getIdea(42 - 1)
+    << std::endl;
+
+  printBabyChickSeparator();
+
+  delete snoopy;
+
+  printTreeSeparator();
+
+  delete scooby;
+
+  printBabyChickSeparator();
+
+  Cat *neko = new Cat;
+
+  printTreeSeparator();
+
+  Cat *kitty = new Cat(*neko);
+
+  printBabyChickSeparator();
+
+  Brain *nekoBrain = neko->getBrain();
+  Brain *kittyBrain = kitty->getBrain();
+
+  nekoBrain->setIdea(42 - 1, "Moulinette");
+  kittyBrain->setIdea(42 - 1, "Norminette");
+
+  std::cout
+    << "🚀 Neko brain - address: " << nekoBrain
+    << " - 42 idea: " << nekoBrain->getIdea(42 - 1)
+    << std::endl;
+
+  std::cout
+    << "🚀 Kitty brain - address: " << kittyBrain
+    << " - 42 idea: " << kittyBrain->getIdea(42 - 1)
+    << std::endl;
+
+  printBabyChickSeparator();
+
+  delete neko;
+
+  printTreeSeparator();
+  
+  delete kitty;
+}
+
+static void printBabyChickSeparator(void) {
   std::cout
     << std::endl
     << "🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣🐥🐣"
+    << std::endl
+    << std::endl;
+}
+
+static void printTreeSeparator(void) {
+  std::cout
+    << "🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲🌳🌲"
+    << std::endl;
+}
+
+static void printFireSeparator(void) {
+  std::cout
+    << std::endl
+    << "🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥🧨🔥"
     << std::endl
     << std::endl;
 }
