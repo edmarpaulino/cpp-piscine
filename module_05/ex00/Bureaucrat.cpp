18f0184b6d06ce6811bd8dc39ae1c0530feb2fec
Edmar Paulino <edmarpaulino9@gmail.com>
2023-08-02 21:12:19 -0300

    chore: update bureaucrat emoji module 05 ex 00

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -6,34 +6,34 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 20:35:55 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/07/31 22:23:44 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/08/02 21:11:25 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(void): _name("Unnamed"), _grade(1) {
-  std::cout << "📑 Bureaucrat default constructor called 🔨" << std::endl;
+  std::cout << "🏛️  Bureaucrat default constructor called 🔨" << std::endl;
 }
 
 Bureaucrat::Bureaucrat(const std::string name, int grade): _name(name) {
-  std::cout << "📑 Bureaucrat constructor called 🪛" << std::endl;
+  std::cout << "🏛️  Bureaucrat constructor called 🪛" << std::endl;
   if (grade < 1) throw GradeTooHighException();
   if (grade > 150) throw GradeTooLowException();
   this->_grade = grade;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &rhs) {
-  std::cout << "📑 Bureaucrat copy constructor called 🛠️" << std::endl;
+  std::cout << "🏛️  Bureaucrat copy constructor called 🛠️" << std::endl;
   *this = rhs;
 }
 
 Bureaucrat::~Bureaucrat(void) {
-  std::cout << "📑 Bureaucrat destructor called 💣" << std::endl;
+  std::cout << "🏛️  Bureaucrat destructor called 💣" << std::endl;
 }
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs) {
-  std::cout << "📑 Bureaucrat assignment operator called 🔧" << std::endl;
+  std::cout << "🏛️  Bureaucrat assignment operator called 🔧" << std::endl;
   if (this != &rhs) {
     const_cast<std::string&>(this->_name) = rhs._name;
     this->_grade = rhs._grade;
@@ -60,15 +60,15 @@ void Bureaucrat::decrementGrade(void) {
 }
 
 const char *Bureaucrat::GradeTooHighException::what(void) const throw() {
-  return "📑 Bureaucrat grade is too high ⬆️ ⚠️";
+  return "🏛️  Bureaucrat grade is too high ⬆️ ⚠️";
 }
 
 const char *Bureaucrat::GradeTooLowException::what(void) const throw() {
-  return "📑 Bureaucrat grade is too low ⬇️ ⚠️";
+  return "🏛️  Bureaucrat grade is too low ⬇️ ⚠️";
 }
 
 std::ostream &operator<<(std::ostream &o, const Bureaucrat &rhs) {
-  o << "📑 " << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
+  o << "🏛️  " << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
 
   return o;
 }
