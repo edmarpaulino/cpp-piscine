cc6481519393b15b425912713cb249f9c5aa585b
Edmar Paulino <edmarpaulino9@gmail.com>
2023-08-03 21:45:49 -0300

    chore: adjust spacing module 05 ex 02

--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 20:32:28 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/08/03 20:07:20 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/08/03 21:45:06 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,32 @@ static void printLineSeparator(void);
 
 int main(void) {
   printPenSeparator();
+  
   {
     ShrubberyCreationForm form("Albuquerque");
     Bureaucrat kim("Kim Wexler", 137);
     Bureaucrat saul("Saul Goodman", 146);
     testForm(&form, &kim, &saul);
   }
+  
   printPenSeparator();
+  
   {
     RobotomyRequestForm form("Heisenberg");
     Bureaucrat kim("Kim Wexler", 45);
     Bureaucrat saul("Saul Goodman", 73);
     testForm(&form, &kim, &saul);
   }
+  
   printPenSeparator();
+  
   {
     PresidentialPardonForm form("Jesse Pinkman");
     Bureaucrat kim("Kim Wexler", 1);
     Bureaucrat saul("Saul Goodman", 150);
     testForm(&form, &kim, &saul);
   }
+  
   printPenSeparator();
   
   return 0;
@@ -106,8 +112,6 @@ static void printPenSeparator(void) {
     << std::endl
     << std::endl;
 }
-
-
 
 static void printLineSeparator(void) {
   std::cout << std::endl;
