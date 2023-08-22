94c30c254d910b2b186a8ab7553af1931fdf3b57
Edmar Paulino <edmarpaulino9@gmail.com>
2023-08-22 20:27:45 -0300

    feat: finish module 07 ex 02

---
+++ module_07/ex02/Array.hpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 22:00:48 by edpaulin          #+#    #+#             */
+/*   Updated: 2023/08/22 20:02:33 by edpaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array {
+public:
+  Array(void): _arr(new T[0]), _size(0) {};
+  
+  Array(unsigned int n): _arr(new T[n]), _size(n) {};
+  
+  Array(Array const &rhs): _arr(new T[rhs._size]), _size(rhs._size) { *this = rhs; };
+  
+  ~Array(void) { delete [] _arr; };
+
+  Array &operator=(Array const &rhs) {
+    if (this != &rhs) {
+      delete [] _arr;
+      _arr = new T[rhs._size];
+      _size = rhs._size;
+      for (unsigned int i = 0; i < _size; i++)
+        _arr[i] = rhs._arr[i];
+    }
+
+    return *this;
+  };
+
+  T &operator[](unsigned int i) const {
+    if (i >= _size) throw OutOfBoundsException();
+    
+    return _arr[i];
+  };
+
+  class OutOfBoundsException: public std::exception {
+    virtual const char *what() const throw() {
+      return "Error: index out of bounds";
+    };
+  };
+
+  unsigned int size(void) const { return _size; };
+
+private:
+  T *_arr;
+  unsigned int _size;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Array<T> &rhs) {
+  out << "{ ";
+
+  for (unsigned int i = 0; i < rhs.size(); i++) {
+    out << rhs[i];
+    if (i != rhs.size() - 1) {
+      out << ", ";
+    }
+  }
+
+  out << " }";
+
+  return out;
+}
+
+#endif
---
+++ module_07/ex02/main.cpp
@@ -0,0 +1,227 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 22:48:31 by edpaulin          #+#    #+#             */
+/*   Updated: 2023/08/22 20:19:46 by edpaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+#include "Array.hpp"
+
+static void printRocketSeparator(void);
+static void printFilmFrameSeparator(void);
+static void testSuite(void);
+static void testExceptions(void);
+static void testBool(void);
+static void testChar(void);
+static void testInt(void);
+static void testFloat(void);
+static void testDouble(void);
+static void testString(void);
+
+int main(void) {
+  testSuite();
+  
+  return 0;
+}
+
+static void printRocketSeparator(void) {
+  std::cout
+    << std::endl
+    << "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+    << std::endl
+    << std::endl;
+}
+
+static void printFilmFrameSeparator(void) {
+  std::cout << "🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️🎞️" << std::endl;
+}
+
+static void testSuite(void) {
+  testExceptions();
+
+  printRocketSeparator();
+  
+  testBool();
+
+  printRocketSeparator();
+
+  testChar();
+  
+  printRocketSeparator();
+  
+  testInt();
+  
+  printRocketSeparator();
+  
+  testFloat();
+  
+  printRocketSeparator();
+  
+  testDouble();
+  
+  printRocketSeparator();
+  
+  testString();
+  
+  printRocketSeparator();
+}
+
+static void testExceptions(void) {
+  std::cout << "🧪 EXCEPTIONS 🧪" << std::endl << std::endl;
+
+  try {
+    Array<int> arr;
+
+    std::cout << "arr.size(): " << arr.size() << std::endl;
+
+    arr[-42] = -42;
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  printFilmFrameSeparator();
+
+  try {
+    Array<int> arr;
+
+    std::cout << "arr.size(): " << arr.size() << std::endl;
+
+    arr[42] = 42;
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  printFilmFrameSeparator();
+
+  try {
+    Array<int> arr(42);
+
+    std::cout << "arr.size(): " << arr.size() << std::endl;
+
+    arr[42] = 42;
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  printFilmFrameSeparator();
+}
+
+static void testBool(void) {
+  std::cout << "🧪 BOOL 🧪" << std::endl << std::endl;
+  
+  Array<bool> arr(2);
+  
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+  
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = i % 2;
+  }
+
+  std::cout << arr << std::endl;
+
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = (i + 1) % 2;
+  }
+
+  std::cout << arr << std::endl;
+}
+
+static void testChar(void) {
+  std::cout << "🧪 CHAR 🧪" << std::endl << std::endl;
+  
+  Array<char> arr(3);
+  
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = i + 65;
+  }
+
+  std::cout << arr << std::endl;
+
+  arr[arr.size() - 1] = 'Z';
+
+  std::cout << arr << std::endl;
+}
+
+static void testInt(void) {
+  std::cout << "🧪 INT 🧪" << std::endl << std::endl;
+  
+  Array<int> arr(4);
+
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = 100 * (i + 1);
+  }
+
+  std::cout << arr << std::endl;
+
+  arr[arr.size() / 2] = 42;
+  
+  std::cout << arr << std::endl;
+}
+
+static void testFloat(void) {
+  std::cout << "🧪 FLOAT 🧪" << std::endl << std::endl;
+  
+  Array<float> arr(5);
+
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = i + 1 + ((float)(i + 1) / 10);
+  }
+
+  std::cout << arr << std::endl;
+
+  arr[arr.size() / 2] = 42.42f; 
+
+  std::cout << arr << std::endl;
+}
+
+static void testDouble(void) {
+  std::cout << "🧪 DOUBLE 🧪" << std::endl << std::endl;
+
+  Array<double> arr(6);
+
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = i + 1 + ((double)(i + 1) / 10);
+  }
+
+  std::cout << arr << std::endl;
+
+  arr[arr.size() / 3] = 42.42;
+
+  std::cout << arr << std::endl;
+}
+
+static void testString(void) {
+  std::cout << "🧪 STRING 🧪" << std::endl << std::endl;
+  
+  Array<std::string> arr(7);
+
+  std::cout << "arr.size(): " << arr.size() << std::endl;
+  
+  for (unsigned int i = 0; i < arr.size(); i++) {
+    arr[i] = i + 1 + '0';
+  }
+
+  std::cout << arr << std::endl;
+
+  arr[arr.size() - 3] = "42";
+
+  std::cout << arr << std::endl;
+}
