c5530182fed2c93758380d636053eb577f08a4a7
Edmar Paulino <edmarpaulino9@gmail.com>
2023-09-03 20:42:31 -0300

    test: ensure easyfind returns -1 if the second param is not in the container

--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 18:49:46 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/09/03 20:30:15 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/09/03 20:40:56 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,21 @@ static void testNotFound(void) {
     const std::string description = "Should return -1 if the first param is an empty container";
     const std::vector<int> vec;
     const ssize_t result = easyfind(vec,42);
+    if (result == -1) {
+      std::cout << "PASS";
+    } else {
+      std::cout << "FAIL";
+    }
+    std::cout << " - " << description << std::endl;
+  }
+  {
+    const std::string description = "Should return -1 if the second param is not in the container";
+    std::vector<int> vec;
+    for (int i = -10000; i < 10000; i++) {
+      if (i == 42) continue;
+      vec.push_back(i);
+    }
+    const ssize_t result = easyfind(vec, 42);
     if (result == -1) {
       std::cout << "PASS";
     } else {
