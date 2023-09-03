1f8dff52569955bea57922a345d36421746af66f
Edmar Paulino <edmarpaulino9@gmail.com>
2023-09-03 20:29:51 -0300

    feat: ensure easyfind returns -1 if the first param is an empty container

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 19:30:23 by edpaulin          #+#    #+#             */
+/*   Updated: 2023/09/03 20:26:27 by edpaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <sys/types.h>
+
+template <typename T>
+ssize_t  easyfind(T container, int value) {
+  (void)container;
+  (void)value;
+  return -1;
+}
+
+#endif /* EASYFIND_HPP */
