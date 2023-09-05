304a7495fc8e6dd940f53e88217e6eb52fe2caf6
Edmar Paulino <edmarpaulino9@gmail.com>
2023-09-05 20:00:54 -0300

    feat: ensure easyfind returns an iterator if the second param is found

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 19:30:23 by edpaulin          #+#    #+#             */
-/*   Updated: 2023/09/03 21:03:06 by edpaulin         ###   ########.fr       */
+/*   Updated: 2023/09/05 18:38:53 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 #define EASYFIND_HPP
 
 #include <exception>
+#include <algorithm>
+#include <iterator>
 
 template <typename T>
-typename T::iterator  easyfind(T &container, int value) {
-  throw std::runtime_error("value not found");
-  (void)container;
-  (void)value;
+typename T::iterator easyfind(T &container, int value) {
+  typename T::iterator result = std::find(container.begin(), container.end(), value);
+  if (result == container.end()) {
+    throw std::runtime_error("value not found");
+  }
+  return result;
 }
 
 #endif /* EASYFIND_HPP */
